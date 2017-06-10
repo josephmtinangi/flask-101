8179be2c6f515370ccaab5ec5c7ce1cb8b17fe73
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-10 16:09:30 +0300

    Error handling

--- app.py
+++ app.py
@@ -49,5 +49,10 @@ def log_the_user_in(username):
     return redirect(url_for('hello'))
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
