a4567e66a6364f9d97e20554f5cb321b8461430e
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-10 15:12:49 +0300

    Pass integer only parameters

--- app.py
+++ app.py
@@ -18,5 +18,10 @@ def show_user_profile(username):
     return 'User %s' % username
 
 
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+
 if __name__ == '__main__':
     app.run(debug=True)
