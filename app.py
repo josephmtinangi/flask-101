e636c9f4cdc675c60a61235c5b3ca700813f1e64
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-10 15:48:58 +0300

    The request object

--- app.py
+++ app.py
@@ -23,12 +23,16 @@ def show_post(post_id):
     return 'Post %d' % post_id
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    error = None
     if request.method == 'POST':
-        return 'do the login'
-    else:
-        return 'show login form'
+        if valid_login(request.form['username'], request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+
+    return render_template('login.html', error=error)
 
 
 @app.route('/hello/')
@@ -37,9 +41,13 @@ def hello(name=None):
     return render_template('hello.html', name=name)
 
 
-with app.test_request_context('/hello', method='POST'):
-    assert request.path == '/hello'
-    assert request.method == 'POST'
+def valid_login():
+    return True
+
+
+def log_the_user_in():
+    return True
+
 
 if __name__ == '__main__':
     app.run(debug=True)
